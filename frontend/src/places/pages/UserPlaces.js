49fcdd42c381520fefea86363d4eda9fcdb00ecd
Kirk Paradis <kirk.paradis@gmail.com>
2021-07-07 13:41:06 +0900

    Rendering User Places

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
@@ -29,7 +30,11 @@ const DUMMY_PLACES = [
   },
 ];
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const filteredPlaces = DUMMY_PLACES.filter(
+    (place) => place.creator === userId
+  );
+  return <PlaceList items={filteredPlaces} />;
 };
 
 export default UserPlaces;
