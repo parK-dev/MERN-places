e7061d03485b3a81edeb1b1dead94590bef35c4a
Kirk Paradis <kirk.paradis@gmail.com>
2021-07-13 17:04:56 +0900

    changed routes to env

--- frontend/src/places/pages/EditPlace.js
+++ frontend/src/places/pages/EditPlace.js
@@ -39,7 +39,7 @@ const EditPlace = () => {
     const fetchPlace = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
         setLoadedPlace(responseData.place);
         setFormData(
@@ -67,7 +67,7 @@ const EditPlace = () => {
 
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${placeId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         "PATCH",
         JSON.stringify({
           title: formState.inputs.title.value,
