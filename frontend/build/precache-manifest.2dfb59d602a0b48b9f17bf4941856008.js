9fff7621f6f44d1172615dfcf6e0cea2dea84b04
Kirk Paradis <kirk.paradis@gmail.com>
2021-07-13 20:59:38 +0900

    prod build, frontend

---
+++ frontend/build/precache-manifest.2dfb59d602a0b48b9f17bf4941856008.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44ef877a642138dad38d7bf6696dd1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be015e0c7ceeff3781d",
+    "url": "/static/css/0.a798c02f.chunk.css"
+  },
+  {
+    "revision": "0efc8e771a15a8b45b07",
+    "url": "/static/css/4.fee6ba15.chunk.css"
+  },
+  {
+    "revision": "516a5896bf2af3d7144b",
+    "url": "/static/css/5.2982e04b.chunk.css"
+  },
+  {
+    "revision": "74cc659c9200d841e8c5",
+    "url": "/static/css/6.b6679c46.chunk.css"
+  },
+  {
+    "revision": "4a0bb9c40c05e17d5e37",
+    "url": "/static/css/7.2c8f7937.chunk.css"
+  },
+  {
+    "revision": "5849331760451b2de7fc",
+    "url": "/static/css/8.5fbb82ee.chunk.css"
+  },
+  {
+    "revision": "cce00762ea0d44e3218b",
+    "url": "/static/css/main.1dde2701.chunk.css"
+  },
+  {
+    "revision": "6be015e0c7ceeff3781d",
+    "url": "/static/js/0.5b0c51e9.chunk.js"
+  },
+  {
+    "revision": "e292a749d8d98ad7f458",
+    "url": "/static/js/3.f8f9d4bd.chunk.js"
+  },
+  {
+    "revision": "0efc8e771a15a8b45b07",
+    "url": "/static/js/4.e7bb46b3.chunk.js"
+  },
+  {
+    "revision": "516a5896bf2af3d7144b",
+    "url": "/static/js/5.22ae1320.chunk.js"
+  },
+  {
+    "revision": "74cc659c9200d841e8c5",
+    "url": "/static/js/6.07c899a5.chunk.js"
+  },
+  {
+    "revision": "4a0bb9c40c05e17d5e37",
+    "url": "/static/js/7.20a07cf6.chunk.js"
+  },
+  {
+    "revision": "5849331760451b2de7fc",
+    "url": "/static/js/8.274ddb3e.chunk.js"
+  },
+  {
+    "revision": "cce00762ea0d44e3218b",
+    "url": "/static/js/main.d0116efa.chunk.js"
+  },
+  {
+    "revision": "54d4b45bdea85a7766b8",
+    "url": "/static/js/runtime-main.c0de758c.js"
+  }
+]);
